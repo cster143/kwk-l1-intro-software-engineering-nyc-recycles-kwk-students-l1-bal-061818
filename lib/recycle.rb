6aa2b9eabd6967789bb768d253aee62516ddec0e
cster143 <cster143@gmail.com>
2018-06-21 18:27:35 +0000

    Done.

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -1,10 +1,10 @@
-###### Code Your Solutions Here!!!
-
-#Your code should work with a hash that looks like the one below (in comments).
-#The specs (which call your tests) are using these exact hashes 
-# We haven't hard coded the hashes in the tests, 
-#they're dynamically brought in from the NYC Open Data API
-
+# ###### Code Your Solutions Here!!!
+#
+# #Your code should work with a hash that looks like the one below (in comments).
+# #The specs (which call your tests) are using these exact hashes
+# # We haven't hard coded the hashes in the tests,
+# #they're dynamically brought in from the NYC Open Data API
+#
 # first_recycle_bin =  {
 #   "address"=>"E 227 St/Bronx River Pkway",
 #   "site_type"=>"Subproperty",
@@ -12,23 +12,71 @@
 #   "borough"=>"Bronx",
 #   "latitude"=>"40.890848989",
 #   "park_site_name"=>"227th St. Plgd"
-# }
+}
+def address
+  first_recycle_bin =  {
+    "address"=>"E 227 St/Bronx River Pkway",
+    "site_type"=>"Subproperty",
+    "longitude"=>"-73.864223918",
+    "borough"=>"Bronx",
+    "latitude"=>"40.890848989",
+    "park_site_name"=>"227th St. Plgd"
+  }
+  puts first_recycle_bin[address]
+end
+address
+
+def borough
+  first_recycle_bin =  {
+    "address"=>"E 227 St/Bronx River Pkway",
+    "site_type"=>"Subproperty",
+    "longitude"=>"-73.864223918",
+    "borough"=>"Bronx",
+    "latitude"=>"40.890848989",
+    "park_site_name"=>"227th St. Plgd"
+  }
+  puts first_recycle_bin[borough]
+end
+borough
 
 
-#1. Define a method `address`, that takes a hash as an argument.
-#This method should return the value of the address key in the hash.
+def all_keys
+  first_recycle_bin =  {
+    "address"=>"E 227 St/Bronx River Pkway",
+    "site_type"=>"Subproperty",
+    "longitude"=>"-73.864223918",
+    "borough"=>"Bronx",
+    "latitude"=>"40.890848989",
+    "park_site_name"=>"227th St. Plgd"
+  }
+  puts first_recycle_bin.keys
+end
+all_keys
 
-#2. Define a method called `borough` that takes a hash as an argument.
-# This method should return the value of the borough key in the hash.
 
-#3. Define a method called `all_keys` that takes a hash as an argument.
-#This method should print out each key in the hash. 
-#You should use the `each_key` method to iterate over your hash and print out each key.
+def all_data
+  first_recycle_bin =  {
+    "address"=>"E 227 St/Bronx River Pkway",
+    "site_type"=>"Subproperty",
+    "longitude"=>"-73.864223918",
+    "borough"=>"Bronx",
+    "latitude"=>"40.890848989",
+    "park_site_name"=>"227th St. Plgd"
+  }
+  puts first_recycle_bin.values
+end
+all_data
 
-#4. Define a method called `all_data` that takes a hash as an argument.
-#This method should print out each value in the hash.
-#You should use the `each_value` method to iterate over your hash and print out each value.
-
-#5. Define a method called `key_and_data` that takes a hash as an argument.
-#This method should print out each key and value in the hash in a sentence like this: `The #{key} is #{value}`
-#You should use the `each` method to iterate over your hash to print out that sentence
+def key_and_data
+  first_recycle_bin =  {
+    "address"=>"E 227 St/Bronx River Pkway",
+    "site_type"=>"Subproperty",
+    "longitude"=>"-73.864223918",
+    "borough"=>"Bronx",
+    "latitude"=>"40.890848989",
+    "park_site_name"=>"227th St. Plgd"
+  }
+ first_recycle_bin.each do |x,y|
+   puts "The #{x} is #{y}"
+end
+key_and_data
