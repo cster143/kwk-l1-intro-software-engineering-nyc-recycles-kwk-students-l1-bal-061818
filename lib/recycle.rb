0692ce285a5b1fa8a8a9c083797ade5d380e9f7a
cster143 <cster143@gmail.com>
2018-06-21 18:23:49 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -29,9 +29,11 @@ def all_keys
 end
 all_keys
 
-#4. Define a method called `all_data` that takes a hash as an argument.
-#This method should print out each value in the hash.
-#You should use the `each_value` method to iterate over your hash and print out each value.
+
+def all_data
+  puts first_recycle_bin.values
+end
+all_data
 
 #5. Define a method called `key_and_data` that takes a hash as an argument.
 #This method should print out each key and value in the hash in a sentence like this: `The #{key} is #{value}`
